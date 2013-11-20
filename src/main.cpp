0933f93200036628117dbf7eda2986e41a21b6aa
Mikey Chen <jajankenn@gmail.com>
2013-11-19 21:15:12 -0500

    added glut related things

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,62 @@
 #include "main.h"
 
-int main()
+using glut_window_const::width;
+using glut_window_const::height;
+using std::cout;
+using std::endl;
+
+
+
+void reshape(int w, int h)
 {
-	return 0;
+    width = w;
+    height = h;
+    glBindFramebuffer(GL_FRAMEBUFFER,0);
+    glViewport(0,0,(GLsizei)w,(GLsizei)h);
+}
+
+void display(void)
+{
+	glutPostRedisplay();
+    glutSwapBuffers();
+}
+
+void keyboard(unsigned char key, int x, int y) 
+{
+	switch(key) 
+	{
+		case(27):
+			exit(0);
+			break;
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    width = 1280;
+    height = 720;
+    glutInitWindowSize(width,height);
+	glutCreateWindow("GAIA Frame");
+    glewInit();
+    GLenum err = glewInit();
+    if (GLEW_OK != err)
+    {
+        /* Problem: glewInit failed, something is seriously wrong. */
+        cout << "glewInit failed, aborting." << endl;
+        exit (1);
+    }
+	
+    cout << "Status: Using GLEW " << glewGetString(GLEW_VERSION) << endl;
+    cout << "OpenGL version " << glGetString(GL_VERSION) << " supported" << endl;
+
+	glutDisplayFunc(display);
+    glutReshapeFunc(reshape);	
+    glutKeyboardFunc(keyboard);
+    //glutMouseFunc(mouse);
+    //glutMotionFunc(motion);
+
+    glutMainLoop();
+    return 0;
 }
--- src/main.h
+++ src/main.h
@@ -2,10 +2,22 @@
 #define MAIN_H
 
 #include <GL/glew.h>
+#include <GL/glut.h>
 #include <glm/glm.hpp>
 #include <vector>
 #include <cstring>
+#include <iostream>
+#include <string>
 #include "SOIL.h"
+
+
+namespace glut_window_const {
+	int width;
+	int height;
+}
+
+void reshape(int w, int h);
+void display(void);
 
 
 #endif
