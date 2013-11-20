890b9cbf0395ccaf18014343bb200631675d0225
Mikey Chen <jajankenn@gmail.com>
2013-11-19 20:48:36 -0500

    set up project complete

---
+++ Gaia/Gaia/camera.h
@@ -0,0 +1,19 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include "glm/glm.hpp"
+
+class Camera {
+
+public:
+
+private:
+	float rx;
+	float ry;
+	float rz;
+	glm::vec2 pos;
+
+
+}
+
+#endif
--- src/main.h
+++ src/main.h
@@ -5,6 +5,7 @@
 #include <glm/glm.hpp>
 #include <vector>
 #include <cstring>
+#include "SOIL.h"
 
 
 #endif
